22848aa186a9c08afe9fc966fe4bbdab96a2dbec
cloudhuh <claudiu.huhulia@gmail.com>
2023-09-01 22:59:02 +0200

    added edit view

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -1,19 +1,21 @@
 @extends('layouts.app')
-@section('title', 'Create Project')
+@section('title', 'Modifica Project')
 
 @section('content')
     <header>
-        <h1>Crea Projetto</h1>
+        <h1>Modifica Projetto</h1>
     </header>
     <hr>
-    <form action="{{ route('admin.projects.store') }}" method="POST">
+    <form action="{{ route('admin.projects.update', $project) }}" method="POST">
         @csrf
+        @method('PUT')
         <div class="row">
             <div class="col-6">
                 <div class="mb-3">
                     <label for="name" class="form-label">Name</label>
                     <input type="text" class="form-control" id="name" name="name"
-                        placeholder="Inserisci il nome del progetto" value="{{ old('name') }}" maxlength="50" required>
+                        placeholder="Inserisci il nome del progetto" value="{{ old('name', $project->name) }}"
+                        maxlength="50" required>
                 </div>
             </div>
             <div class="col-6">
@@ -21,25 +23,25 @@
                 <div class="mb-3">
                     <label for="slug" class="form-label">Slug</label>
                     <input type="text" class="form-control" id="slug" disabled
-                        value="{{ Str::slug(old('name'), '-') }}">
+                        value="{{ Str::slug(old('name', $project->name), '-') }}">
                 </div>
             </div>
 
             <div class="col-12">
                 <div class="mb-3">
                     <label for="content" class="form-label">Content</label>
-                    <textarea class="form-control" id="content" name="content" rows="10" required>{{ old('content') }}</textarea>
+                    <textarea class="form-control" id="content" name="content" rows="10" required>{{ old('content', $project->content) }}</textarea>
                 </div>
             </div>
             <div class="col-11">
                 <div class="mb-3">
                     <label for="image" class="form-label">Url dell'immagine</label>
-                    <input type="url" class="form-control" id="image" name="image" value="{{ old('image') }}"
-                        placeholder="Insersisci un url valido">
+                    <input type="url" class="form-control" id="image" name="image"
+                        value="{{ old('image', $project->image) }}" placeholder="Insersisci un url valido">
                 </div>
             </div>
             <div class="col-1">
-                <img src="{{ old('image', 'https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=') }}"
+                <img src="{{ old('image', $project->image ?? 'https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=') }}"
                     alt="preview" class="img-fluid my-2" id="image-preview">
             </div>
             <hr>
